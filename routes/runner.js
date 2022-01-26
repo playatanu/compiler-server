228f566fc2a81d153dd454b90326e797807afb30
playatanu <playatanu@gmail.com>
2022-01-26 21:21:37 +0530

    update

--- routes/runner.js
+++ routes/runner.js
@@ -2,7 +2,7 @@ const compiler = require("../functions/compiler");
 const authMiddleware = require("../middlewares/authMiddleware");
 const router = require("express").Router();
 
-router.route("/").post(authMiddleware, async (req, res) => {
+router.route("/").post(async (req, res) => {
   const output = await compiler(req.body.code, req.body.input, req.body.len);
   res.send(output);
 });
